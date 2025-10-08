9fc4cc53363206f57492f53777150763d5cd702c
explorerRhythm <labibhasan342@gmail.com>
2025-10-08 22:18:49 +0600

    add margin top to be more modern

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const answer = document.querySelectorAll(".answer");
 const toggleButton = document.querySelectorAll(".question button");
 const question = document.querySelectorAll(".question");
+const pushLine = document.querySelector("#after-faq");
 for (let i = 0; i < answer.length; i++) {
   toggleButton[i].addEventListener("click", function (e) {
     e.stopPropagation();
     if (answer[i].style.display === "none" || answer[i].style.display === "") {
       answer[i].style.display = "block";
+      pushLine.style.marginTop = "20rem";
     } else {
       answer[i].style.display = "none";
+      pushLine.style.marginTop = "10rem";
     }
   });
 }
@@ -18,8 +21,10 @@ question.forEach((para, index) => {
       answer[index].style.display === ""
     ) {
       answer[index].style.display = "block";
+      pushLine.style.marginTop = "20rem";
     } else {
       answer[index].style.display = "none";
+      pushLine.style.marginTop = "10rem";
     }
   });
 });
